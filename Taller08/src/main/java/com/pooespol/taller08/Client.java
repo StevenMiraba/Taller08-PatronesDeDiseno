c4170876ef568dd5770ff7f55cd3fea5041d7be2
StevenMiraba <150202191+StevenMiraba@users.noreply.github.com>
2024-07-17 17:45:10 -0500

    Add files via upload

---
+++ Taller08/src/main/java/com/pooespol/taller08/Client.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pooespol.taller08;
+
+/**
+ *
+ * @author STEVEN
+ */
+public class Client {
+    
+}
