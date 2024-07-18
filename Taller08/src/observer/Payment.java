a180083a014f9aabfeec3050320f06dd69308cee
StevenMiraba <wsmiraba@espol.edu.ec>
2024-07-17 19:18:23 -0500

    payment class

--- Taller08/src/observer/Payment.java
+++ Taller08/src/observer/Payment.java
@@ -1,5 +1,25 @@
 package observer;
 
 public class Payment {
+    public double amount;
+    private NotificationStrategy notification;
+
+    public boolean pay(double amount){
+        if(amount!=0){
+            return True;
+        }
+        else{
+            return False;
+        }
+    }
+
+    public void setNotificationStrategy(NotificationStrategy not){
+        //code
+    }
+
+    public void processPayment(double amount){
+        //code
+    }
+
 
 }
